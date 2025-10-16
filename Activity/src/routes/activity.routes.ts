dd60df8f439ac32ea0609604b24066b69c472118
Neha471 <nehashrivastava531@gmail.com>
2025-10-17 00:11:36 +0530

    feat:del-activity-controller, create-category-routes

--- Activity/src/routes/activity.routes.ts
+++ Activity/src/routes/activity.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { activityController } from '../controllers/activity.controller';
 import { authenticate } from '../middlewares/auth.middleware';
+import { enhancedActivityController } from '../controllers/enhanced-activity.controller';
 
 const router = Router();
 
@@ -8,18 +8,33 @@ const router = Router();
 router.use(authenticate);
 
 // Create a new activity
-router.post('/', activityController.createActivity.bind(activityController));
+router.post('/', enhancedActivityController.createActivity.bind(enhancedActivityController));
 
 // Get all activities for the authenticated user
-router.get('/', activityController.getAllActivities.bind(activityController));
+router.get('/', enhancedActivityController.getAllActivities.bind(enhancedActivityController));
 
 // Get a specific activity by ID
-router.get('/:id', activityController.getActivity.bind(activityController));
+router.get('/:id', enhancedActivityController.getActivity.bind(enhancedActivityController));
 
 // Update an activity
-router.patch('/:id', activityController.updateActivity.bind(activityController));
+router.patch('/:id', enhancedActivityController.updateActivity.bind(enhancedActivityController));
 
 // Delete an activity
-router.delete('/:id', activityController.deleteActivity.bind(activityController));
+router.delete('/:id', enhancedActivityController.deleteActivity.bind(enhancedActivityController));
+
+// Create a new category
+router.post('/category', enhancedActivityController.createCategory.bind(enhancedActivityController));
+
+// Get all categories for the authenticated user
+router.get('/category/all', enhancedActivityController.getAllCategories.bind(enhancedActivityController));
+
+// Get a specific category by ID
+router.get('/category/:id', enhancedActivityController.getCategoryById.bind(enhancedActivityController));
+
+// Update a category
+router.patch('/category/:id', enhancedActivityController.updateCategory.bind(enhancedActivityController));
+
+// Delete a category
+router.delete('/category/:id', enhancedActivityController.deleteCategory.bind(enhancedActivityController));
 
 export default router;
